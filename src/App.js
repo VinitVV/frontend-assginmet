ff7f4b78802f64565a532e46ed4751707abad9ef
Vinit Variyani <96524088+VinitVV@users.noreply.github.com>
2023-08-19 15:31:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import KanbanBoard from './components/KanbanBoard';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 function App() {
